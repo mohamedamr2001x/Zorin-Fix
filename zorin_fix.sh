f51cc852c5a1c2350f46d2ca5ccd9bf7b4b83c2a
mohamedamr2001x <116591577+mohamedamr2001x@users.noreply.github.com>
2024-08-02 08:28:57 +0300

    Update zorin_fix.sh

--- zorin_fix.sh
+++ zorin_fix.sh
@@ -1,26 +1,5 @@
 #!/bin/bash
 
-echo "Updating package list..."
-sudo apt update
-
-# Upgrade all packages
-echo "Upgrading packages..."
-sudo apt upgrade -y
-
-
-echo "Installing git if not installed"
-
-sudo apt install git -y
-
-echo "Cloning repo"
-
-git clone https://github.com/mohamedamr2001x/Zorin-Fix.git
-
-echo "Giving execute permissions and read permissions"
-
-sudo chmod 755 -R ./
-
-
 echo "Moving Certificate files"
 
 sudo cp ./Zorin-Fix/Zorin* /usr/local/share/ca-certificates/
